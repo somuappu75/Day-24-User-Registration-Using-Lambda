fcc143de029ca57f657c1b24bb119f02e33d6256
Lenovo <somusp75@gmail.com>
2022-02-01 20:59:37 +0530

    Password  atleast one  number added

--- User-Registration-Using-Lambda/Program.cs
+++ User-Registration-Using-Lambda/Program.cs
@@ -12,6 +12,7 @@ namespace User_Registration_Using_Lambda
             //uc-4 phone number validated
             //Uc-5 validating password -Rule-1 added
             //Uc-6 validating password -Rule-2 added
+            //uc-7 validating password rule -3 atlest one digit added
 
             UserRegex user = new UserRegex();
             user.ValidatingAllFields();
